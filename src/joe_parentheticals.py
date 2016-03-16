a99d21b812eb7fb9755438730a4d9938d3433b85
Joseph McClenahan <jmcclena94@gmail.com>
2016-03-15 18:03:43 -0700

    fixed parenthetical linting errors

--- src/joe_parentheticals.py
+++ src/joe_parentheticals.py
@@ -7,12 +7,14 @@ def parenthetical(parens):
     """Determine if a string has open, balanced, or broken parenthesis"""
     clean_str = re.sub(r'[^(+)]', '', parens)
     while clean_str is not '':
-        if clean_str[0] == ')': return -1
-        if clean_str[-1] == '(': return 1
+        if clean_str[0] == ')':
+            return -1
+        if clean_str[-1] == '(':
+            return 1
         clean_str = clean_str.replace('()', '')
     return 0
 
 
 if __name__ == '__main__':
-    str = sys.argv[1]
-    parenthetical(str)
+    to_parse = sys.argv[1]
+    parenthetical(to_parse)
