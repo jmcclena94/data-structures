1659430c31fbc6e275b3c809647419982c49be7e
GingerQ <qiangwenjing@gmail.com>
2016-03-17 15:06:40 -0700

    fix pqueue sort down function

--- src/pqueue.py
+++ src/pqueue.py
@@ -1,24 +1,32 @@
+# encoding=utf-8
+
+
 class Pqueue(object):
+    """Define class Pqueue."""
 
     def __init__(self, item):
+        """Define initial Pqueue."""
         self.pqueue = []
         self.insert(item)
 
     def insert(self, item):
+        """Insert item tuple in to priority queue."""
         for pair in item:
             self.pqueue.append(pair)
             self._sort_up()
 
     def _sort_up(self):
+        """Sort up insertion by comparing priority key."""
         idx = len(self.pqueue) - 1
-        while (idx-1)//2 >= 0:
-            if self.pqueue[idx][0] > self.pqueue[(idx-1)//2][0]:
-                temp_parent = self.pqueue[(idx-1)//2]
-                self.pqueue[(idx-1)//2] = self.pqueue[idx]
+        while (idx - 1) // 2 >= 0:
+            if self.pqueue[idx][0] > self.pqueue[(idx - 1) // 2][0]:
+                temp_parent = self.pqueue[(idx - 1) // 2]
+                self.pqueue[(idx - 1) // 2] = self.pqueue[idx]
                 self.pqueue[idx] = temp_parent
-            idx = (idx-1)//2
+            idx = (idx - 1) // 2
 
     def pop(self):
+        """Pop out the root and replace th elast node."""
         root = self.pqueue[0]
         self.pqueue[0] = self.pqueue[-1]
         self.pqueue.remove(self.pqueue[-1])
@@ -26,22 +34,22 @@ class Pqueue(object):
         return root[1]
 
     def _sort_down(self):
+        """Sort down the node by comparing priority key."""
         idx = 0
-        while 2*idx + 1 < len(self.pqueue):
-            if 2*idx + 2 == len(self.pqueue):
-                comp_idx = 2*idx + 1
+        while 2 * idx + 1 < len(self.pqueue):
+            if 2 * idx + 2 == len(self.pqueue):
+                comp_idx = 2 * idx + 1
             else:
-                if self.pqueue[2*idx + 1][0] > self.pqueue[2*idx + 2][0]:
-                    comp_idx = 2*idx + 1
+                if self.pqueue[2 * idx + 1][0] > self.pqueue[2 * idx + 2][0]:
+                    comp_idx = 2 * idx + 1
                 else:
-                    comp_idx = 2*idx + 2
+                    comp_idx = 2 * idx + 2
             if self.pqueue[comp_idx][0] > self.pqueue[idx][0]:
                 temp_parent = self.pqueue[idx]
                 self.pqueue[idx] = self.pqueue[comp_idx]
-                temp_parent = self.pqueue[comp_idx]
-                self.pqueue[comp_idx] = self.pqueue[idx]
+                self.pqueue[comp_idx] = temp_parent
             idx = comp_idx
 
     def peek(self):
+        """View the root."""
         return self.pqueue[0]
-
--- src/test_pqueue.py
+++ src/test_pqueue.py
@@ -1,24 +1,28 @@
+# encoding utf-8
+
 
 import pytest
 
 
 INSERT_TABLE = [
     ([(1, 100), (2, 99), (3, 98), (4, 97), (5, 96)],
-    [(5, 96), (4, 97), (2, 99), (1, 100), (3, 98)])
+        [(5, 96), (4, 97), (2, 99), (1, 100), (3, 98)])
 ]
 
 POP_TABLE = [
     ([(1, 100), (2, 99), (3, 98), (4, 97), (5, 96)],
-    [96, 97, 98])
+        [96, 97, 98])
 ]
 
 PEEK_TABLE = [
     ([(1, 100), (2, 99), (3, 98), (4, 97), (5, 96)],
-    [(5, 96)])
+        [(5, 96)])
 ]
+
 
 @pytest.mark.parametrize('value, result', INSERT_TABLE)
 def test_make_pqueue(value, result):
+    """Test insert()."""
     from pqueue import Pqueue
     new_pqueue = Pqueue(value)
     assert new_pqueue.pqueue == result
@@ -26,16 +30,17 @@ def test_make_pqueue(value, result):
 
 @pytest.mark.parametrize('value, result', POP_TABLE)
 def test_pop_pqueue(value, result):
+    """Test pop()."""
     from pqueue import Pqueue
     new_pqueue = Pqueue(value)
     assert new_pqueue.pop() == result[0]
     assert new_pqueue.pop() == result[1]
     assert new_pqueue.pop() == result[2]
 
+
 @pytest.mark.parametrize('value, result', PEEK_TABLE)
 def test_peek_pqueue(value, result):
+    """Test peek()."""
     from pqueue import Pqueue
     new_pqueue = Pqueue(value)
     assert new_pqueue.peek() == result[0]
-
-
