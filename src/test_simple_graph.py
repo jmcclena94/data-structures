db8374e3dcb2ca82c8632e8f01ef208499164029
Joseph McClenahan <jmcclena94@gmail.com>
2016-03-23 14:31:40 -0700

    updated simple graph to add weights

--- src/test_simple_graph.py
+++ src/test_simple_graph.py
@@ -7,7 +7,7 @@ def test_add_node():
     new_graph = SimpleGraph()
     new_graph.add_node('A')
     assert list(new_graph.graph.keys()) == ['A']
-    assert new_graph.graph['A'] == []
+    assert new_graph.graph['A'] == {}
 
 
 def test_nodes():
@@ -20,40 +20,25 @@ def test_nodes():
 def test_add_edge():
     from simple_graph import SimpleGraph
     new_graph = SimpleGraph()
-    new_graph.graph = {'A': ['A', 'B', 'C'], 'B': ['C', 'D']}
-    new_graph.add_edge('B', 'A')
+    new_graph.add_edge('B', 'A', 25)
     assert 'A' in new_graph.graph['B']
-
-
-def test_add_edge_no_nodes():
-    from simple_graph import SimpleGraph
-    new_graph = SimpleGraph()
-    new_graph.graph = {}
-    new_graph.add_edge('B', 'A')
-    assert 'A' in new_graph.graph['B']
+    assert new_graph.graph['B']['A'] == 25
 
 
 def test_edges():
     from simple_graph import SimpleGraph
     new_graph = SimpleGraph()
-    new_graph.graph = {'A': ['A', 'B', 'C']}
-    assert new_graph.edges() == [('A', 'A'), ('A', 'B'), ('A', 'C')]
+    new_graph.add_edge('A', 'B', 25)
+    assert new_graph.edges() == [('A', 'B', 25)]
 
 
 def test_del_node():
     from simple_graph import SimpleGraph
     new_graph = SimpleGraph()
-    new_graph.graph = {'A': ['A', 'B', 'C']}
+    new_graph.add_edge('A', 'B', 25)
+    new_graph.add_edge('B', 'A', 10)
     new_graph.del_node('A')
-    assert new_graph.graph == {}
-
-
-def test_del_node_edge():
-    from simple_graph import SimpleGraph
-    new_graph = SimpleGraph()
-    new_graph.graph = {'A': ['A', 'B', 'C'], 'B': ['A', 'B']}
-    new_graph.del_node('A')
-    assert new_graph.graph == {'B': ['B']}
+    assert new_graph.graph == {'B': {}}
 
 
 def test_del_node_error():
@@ -66,15 +51,15 @@ def test_del_node_error():
 def test_del_edge():
     from simple_graph import SimpleGraph
     new_graph = SimpleGraph()
-    new_graph.graph = {'A': ['A', 'B', 'C']}
+    new_graph.graph = {'A': {'A': 10, 'B': 25}}
     new_graph.del_edge('A', 'B')
-    assert new_graph.graph == {'A': ['A', 'C']}
+    assert new_graph.graph == {'A': {'A': 10}}
 
 
 def test_del_edge_val_error():
     from simple_graph import SimpleGraph
     new_graph = SimpleGraph()
-    new_graph.graph = {'A': ['A', 'B', 'C']}
+    new_graph.graph = {'A': {'A': 10, 'B': 25}}
     with pytest.raises(ValueError):
         new_graph.del_edge('A', 'E')
 
@@ -82,7 +67,7 @@ def test_del_edge_val_error():
 def test_del_edge_key_error():
     from simple_graph import SimpleGraph
     new_graph = SimpleGraph()
-    new_graph.graph = {'A': ['A', 'B', 'C']}
+    new_graph.graph = {'A': {'A': 10, 'B': 25}}
     with pytest.raises(KeyError):
         new_graph.del_edge('G', 'C')
 
@@ -90,7 +75,7 @@ def test_del_edge_key_error():
 def test_has_node():
     from simple_graph import SimpleGraph
     new_graph = SimpleGraph()
-    new_graph.graph = {'A': ['A']}
+    new_graph.graph = {'A': {'A': 15}}
     assert new_graph.has_node('A') is True
     assert new_graph.has_node('B') is False
 
@@ -98,14 +83,14 @@ def test_has_node():
 def test_neighbors():
     from simple_graph import SimpleGraph
     new_graph = SimpleGraph()
-    new_graph.graph = {'A': ['A', 'B', 'C']}
-    assert new_graph.neighbors('A') == ['A', 'B', 'C']
+    new_graph.graph = {'A': {'B': 15}}
+    assert new_graph.neighbors('A') == ['B']
 
 
 def test_neighbors_error():
     from simple_graph import SimpleGraph
     new_graph = SimpleGraph()
-    new_graph.graph = {'A': ['A', 'B', 'C']}
+    new_graph.graph = {'A': {'B': 15}}
     with pytest.raises(KeyError):
         new_graph.neighbors('E')
 
@@ -113,62 +98,78 @@ def test_neighbors_error():
 def test_adjacent():
     from simple_graph import SimpleGraph
     new_graph = SimpleGraph()
-    new_graph.graph = {'A': ['A', 'B', 'C'], 'E': ['A'], 'B': []}
+    new_graph.graph = {'A': {'A': 10, 'B': 25}, 'B': {}, 'C': {}}
     assert new_graph.adjacent('A', 'B') is True
-    assert new_graph.adjacent('A', 'E') is False
+    assert new_graph.adjacent('A', 'C') is False
 
 
 def test_adjacent_error():
     from simple_graph import SimpleGraph
     new_graph = SimpleGraph()
-    new_graph.graph = {'A': ['A', 'B', 'C']}
+    new_graph.graph = {'A': {'A': 10, 'B': 25}, 'B': {}}
     with pytest.raises(KeyError):
         new_graph.adjacent('A', 'E')
+
 
 def test_depth_first_traversal():
     from simple_graph import SimpleGraph
     new_graph = SimpleGraph()
-    new_graph.graph = {'A': ['B', 'C'],
-                       'B': ['D', 'E'],
-                       'D': [],
-                       'E': ['F'],
-                       'C': ['F'],
-                       'F': [],
+    new_graph.graph = {'A': {'B': 10, 'C': 20},
+                       'B': {'D': 5, 'E': 3},
+                       'D': {},
+                       'E': {'F': 13},
+                       'C': {'F': 16},
+                       'F': {},
                        }
-    assert new_graph.dft("A") == ['A', 'C', 'F', 'B', 'E', 'D']
+    visited = new_graph.dft('A')
+    keys = list(new_graph.graph.keys())
+    for val in visited:
+        assert val in keys
+
 
 def test_dft_cyclic():
     from simple_graph import SimpleGraph
     new_graph = SimpleGraph()
-    new_graph.graph = {'A': ['B', 'C'],
-                       'B': ['D', 'E'],
-                       'D': [],
-                       'E': ['F'],
-                       'C': ['F'],
-                       'F': ['A'],
+    new_graph.graph = {'A': {'B': 10, 'C': 20},
+                       'B': {'D': 5, 'E': 3},
+                       'D': {},
+                       'E': {'F': 13},
+                       'C': {'F': 16},
+                       'F': {'A': 100},
                        }
-    assert new_graph.dft("A") == ['A', 'C', 'F', 'B', 'E', 'D']
+    visited = new_graph.dft('A')
+    keys = list(new_graph.graph.keys())
+    for val in visited:
+        assert val in keys
+
 
 def test_breadth_first_traversal():
     from simple_graph import SimpleGraph
     new_graph = SimpleGraph()
-    new_graph.graph = {'A': ['B', 'C'],
-                       'B': ['D', 'E'],
-                       'D': [],
-                       'E': ['F'],
-                       'C': ['F'],
-                       'F': [],
+    new_graph.graph = {'A': {'B': 10, 'C': 20},
+                       'B': {'D': 5, 'E': 3},
+                       'D': {},
+                       'E': {'F': 13},
+                       'C': {'F': 16},
+                       'F': {},
                        }
-    assert new_graph.bft("A") == ['A', 'C', 'B', 'F', 'E', 'D']
+    visited = new_graph.bft('A')
+    keys = list(new_graph.graph.keys())
+    for val in visited:
+        assert val in keys
+
 
 def test_bft_cyclic():
     from simple_graph import SimpleGraph
     new_graph = SimpleGraph()
-    new_graph.graph = {'A': ['B', 'C'],
-                       'B': ['D', 'E'],
-                       'D': [],
-                       'E': ['F'],
-                       'C': ['F'],
-                       'F': ['A'],
+    new_graph.graph = {'A': {'B': 10, 'C': 20},
+                       'B': {'D': 5, 'E': 3},
+                       'D': {},
+                       'E': {'F': 13},
+                       'C': {'F': 16},
+                       'F': {'A': 100},
                        }
-    assert new_graph.bft("A") == ['A', 'C', 'B', 'F', 'E', 'D']
+    visited = new_graph.bft('A')
+    keys = list(new_graph.graph.keys())
+    for val in visited:
+        assert val in keys
