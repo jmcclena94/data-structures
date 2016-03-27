0f79eedc37a4dafbb68fa2ecd4b2a8936b7d9cc2
Joseph McClenahan <jmcclena94@gmail.com>
2016-03-26 18:14:41 -0700

    edited init simple graph tests

--- src/test_simple_graph.py
+++ src/test_simple_graph.py
@@ -13,6 +13,9 @@ def test_add_node():
 def test_nodes():
     from simple_graph import SimpleGraph
     new_graph = SimpleGraph()
+    new_graph.add_edge('A', 'A', 10)
+    new_graph.add_edge('A', 'B', 20)
+    new_graph.add_edge('A', 'C', 30)
     new_graph.graph = {'A': ['A', 'B', 'C']}
     assert new_graph.nodes() == ['A']
 
@@ -51,15 +54,17 @@ def test_del_node_error():
 def test_del_edge():
     from simple_graph import SimpleGraph
     new_graph = SimpleGraph()
-    new_graph.graph = {'A': {'A': 10, 'B': 25}}
+    new_graph.add_edge('A', 'A', 10)
+    new_graph.add_edge('A', 'B', 25)
     new_graph.del_edge('A', 'B')
-    assert new_graph.graph == {'A': {'A': 10}}
+    assert new_graph.graph == {'A': {'A': 10}, 'B': {}}
 
 
 def test_del_edge_val_error():
     from simple_graph import SimpleGraph
     new_graph = SimpleGraph()
-    new_graph.graph = {'A': {'A': 10, 'B': 25}}
+    new_graph.add_edge('A', 'A', 10)
+    new_graph.add_edge('A', 'B', 25)
     with pytest.raises(ValueError):
         new_graph.del_edge('A', 'E')
 
@@ -67,7 +72,8 @@ def test_del_edge_val_error():
 def test_del_edge_key_error():
     from simple_graph import SimpleGraph
     new_graph = SimpleGraph()
-    new_graph.graph = {'A': {'A': 10, 'B': 25}}
+    new_graph.add_edge('A', 'A', 10)
+    new_graph.add_edge('A', 'B', 25)
     with pytest.raises(KeyError):
         new_graph.del_edge('G', 'C')
 
@@ -75,7 +81,7 @@ def test_del_edge_key_error():
 def test_has_node():
     from simple_graph import SimpleGraph
     new_graph = SimpleGraph()
-    new_graph.graph = {'A': {'A': 15}}
+    new_graph.add_edge('A', 'A', 15)
     assert new_graph.has_node('A') is True
     assert new_graph.has_node('B') is False
 
@@ -83,14 +89,14 @@ def test_has_node():
 def test_neighbors():
     from simple_graph import SimpleGraph
     new_graph = SimpleGraph()
-    new_graph.graph = {'A': {'B': 15}}
+    new_graph.add_edge('A', 'B', 15)
     assert new_graph.neighbors('A') == ['B']
 
 
 def test_neighbors_error():
     from simple_graph import SimpleGraph
     new_graph = SimpleGraph()
-    new_graph.graph = {'A': {'B': 15}}
+    new_graph.add_edge('A', 'B', 15)
     with pytest.raises(KeyError):
         new_graph.neighbors('E')
 
@@ -98,7 +104,9 @@ def test_neighbors_error():
 def test_adjacent():
     from simple_graph import SimpleGraph
     new_graph = SimpleGraph()
-    new_graph.graph = {'A': {'A': 10, 'B': 25}, 'B': {}, 'C': {}}
+    new_graph.add_edge('A', 'A', 10)
+    new_graph.add_edge('A', 'B', 25)
+    new_graph.add_node('C')
     assert new_graph.adjacent('A', 'B') is True
     assert new_graph.adjacent('A', 'C') is False
 
@@ -106,7 +114,8 @@ def test_adjacent():
 def test_adjacent_error():
     from simple_graph import SimpleGraph
     new_graph = SimpleGraph()
-    new_graph.graph = {'A': {'A': 10, 'B': 25}, 'B': {}}
+    new_graph.add_edge('A', 'A', 10)
+    new_graph.add_edge('A', 'B', 25)
     with pytest.raises(KeyError):
         new_graph.adjacent('A', 'E')
 
@@ -114,13 +123,12 @@ def test_adjacent_error():
 def test_depth_first_traversal():
     from simple_graph import SimpleGraph
     new_graph = SimpleGraph()
-    new_graph.graph = {'A': {'B': 10, 'C': 20},
-                       'B': {'D': 5, 'E': 3},
-                       'D': {},
-                       'E': {'F': 13},
-                       'C': {'F': 16},
-                       'F': {},
-                       }
+    new_graph.add_edge('A', 'B', 10)
+    new_graph.add_edge('A', 'C', 20)
+    new_graph.add_edge('B', 'D', 5)
+    new_graph.add_edge('B', 'E', 3)
+    new_graph.add_edge('E', 'F', 13)
+    new_graph.add_edge('C', 'F', 16)
     visited = new_graph.dft('A')
     keys = list(new_graph.graph.keys())
     for val in visited:
@@ -130,13 +138,13 @@ def test_depth_first_traversal():
 def test_dft_cyclic():
     from simple_graph import SimpleGraph
     new_graph = SimpleGraph()
-    new_graph.graph = {'A': {'B': 10, 'C': 20},
-                       'B': {'D': 5, 'E': 3},
-                       'D': {},
-                       'E': {'F': 13},
-                       'C': {'F': 16},
-                       'F': {'A': 100},
-                       }
+    new_graph.add_edge('A', 'B', 10)
+    new_graph.add_edge('A', 'C', 20)
+    new_graph.add_edge('B', 'D', 5)
+    new_graph.add_edge('B', 'E', 3)
+    new_graph.add_edge('E', 'F', 13)
+    new_graph.add_edge('C', 'F', 16)
+    new_graph.add_edge('F', 'A', 100)
     visited = new_graph.dft('A')
     keys = list(new_graph.graph.keys())
     for val in visited:
@@ -146,13 +154,12 @@ def test_dft_cyclic():
 def test_breadth_first_traversal():
     from simple_graph import SimpleGraph
     new_graph = SimpleGraph()
-    new_graph.graph = {'A': {'B': 10, 'C': 20},
-                       'B': {'D': 5, 'E': 3},
-                       'D': {},
-                       'E': {'F': 13},
-                       'C': {'F': 16},
-                       'F': {},
-                       }
+    new_graph.add_edge('A', 'B', 10)
+    new_graph.add_edge('A', 'C', 20)
+    new_graph.add_edge('B', 'D', 5)
+    new_graph.add_edge('B', 'E', 3)
+    new_graph.add_edge('E', 'F', 13)
+    new_graph.add_edge('C', 'F', 16)
     visited = new_graph.bft('A')
     keys = list(new_graph.graph.keys())
     for val in visited:
@@ -162,13 +169,13 @@ def test_breadth_first_traversal():
 def test_bft_cyclic():
     from simple_graph import SimpleGraph
     new_graph = SimpleGraph()
-    new_graph.graph = {'A': {'B': 10, 'C': 20},
-                       'B': {'D': 5, 'E': 3},
-                       'D': {},
-                       'E': {'F': 13},
-                       'C': {'F': 16},
-                       'F': {'A': 100},
-                       }
+    new_graph.add_edge('A', 'B', 10)
+    new_graph.add_edge('A', 'C', 20)
+    new_graph.add_edge('B', 'D', 5)
+    new_graph.add_edge('B', 'E', 3)
+    new_graph.add_edge('E', 'F', 13)
+    new_graph.add_edge('C', 'F', 16)
+    new_graph.add_edge('F', 'A', 100)
     visited = new_graph.bft('A')
     keys = list(new_graph.graph.keys())
     for val in visited:
@@ -178,13 +185,13 @@ def test_bft_cyclic():
 def test_bellman():
     from simple_graph import SimpleGraph
     new_graph = SimpleGraph()
-    new_graph.graph = {'A': {'B': 5, 'C': 20},
-                       'B': {'D': 5, 'E': 3},
-                       'D': {},
-                       'E': {'F': 13},
-                       'C': {'F': 16},
-                       'F': {'A': 100},
-                       }
+    new_graph.add_edge('A', 'B', 5)
+    new_graph.add_edge('A', 'C', 20)
+    new_graph.add_edge('B', 'D', 5)
+    new_graph.add_edge('B', 'E', 3)
+    new_graph.add_edge('E', 'F', 13)
+    new_graph.add_edge('C', 'F', 16)
+    new_graph.add_edge('F', 'A', 100)
     distance, path = new_graph.bellman('A', 'F')
     assert distance == 21
     assert path == ['A', 'B', 'E', 'F']
@@ -193,13 +200,13 @@ def test_bellman():
 def test_dijkstra():
     from simple_graph import SimpleGraph
     new_graph = SimpleGraph()
-    new_graph.graph = {'A': {'B': 5, 'C': 20},
-                       'B': {'D': 5, 'E': 3},
-                       'D': {},
-                       'E': {'F': 13},
-                       'C': {'F': 16},
-                       'F': {'A': 100},
-                       }
+    new_graph.add_edge('A', 'B', 5)
+    new_graph.add_edge('A', 'C', 20)
+    new_graph.add_edge('B', 'D', 5)
+    new_graph.add_edge('B', 'E', 3)
+    new_graph.add_edge('E', 'F', 13)
+    new_graph.add_edge('C', 'F', 16)
+    new_graph.add_edge('F', 'A', 100)
     distance, path = new_graph.dijkstra('A', 'F')
     assert distance == 21
     assert path == ['A', 'B', 'E', 'F']
