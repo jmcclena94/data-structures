3cfcb79a560a51c4032c5d4b9bfd8edbc239a35c
JARED SCARR <JAREDSCARR@GMAIL.COM>
2016-04-06 14:56:51 -0700

    Tests added init Node, insert with 1, 2 and 3 values. Passing.

---
+++ src/test_bst.py
@@ -0,0 +1,43 @@
+# coding=utf-8
+
+
+def test_node_class():
+    """Init node."""
+    from bst import Node
+    node = Node(5)
+    assert node.value == 5
+    assert node.right is None
+    assert node.left is None
+    assert node.parent is None
+
+
+def test_insert():
+    """Test if value inserted into the tree."""
+    from bst import Bst
+    new_bst = Bst()
+    new_bst.insert(3)
+    assert new_bst.head.value == 3
+
+
+def test_insert_two_values():
+    """Test insert works for two values."""
+    from bst import Bst
+    new_bst = Bst()
+    new_bst.insert(3)
+    new_bst.insert(4)
+    assert new_bst.head.value == 3
+    assert new_bst.head.right.value == 4
+    assert new_bst.head.left is None
+    
+
+def test_insert_three_values():
+    """Test three values."""
+    from bst import Bst
+    new_bst = Bst()
+    new_bst.insert(3)
+    new_bst.insert(5)
+    new_bst.insert(4)
+    assert new_bst.head.value == 3
+    assert new_bst.head.right.value == 5
+    assert new_bst.head.right.left.value == 4
+
