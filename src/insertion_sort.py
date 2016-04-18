909c42d1d19526280106f995c314796534dca8f3
JARED SCARR <JAREDSCARR@GMAIL.COM>
2016-04-18 15:45:09 -0700

    if __main__ block added to time sorting.

--- src/insertion_sort.py
+++ src/insertion_sort.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+import random
 
 
 def insertion_sort(a_list):
@@ -13,4 +14,45 @@ def insertion_sort(a_list):
     return a_list
 
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
+    """Run in console."""
+    import timeit
+
+    ordered_list = []
+    unordered_list = []
+    reversed_list = []
+
+    for item in range(10000):
+        ordered_list.append(item)
+
+    for item in range(10000):
+        unordered_list.append(random.randint(0, 10000))
+
+    for item in range(10000, 0, -1):
+        reversed_list.append(item)
+
+    def get_ordered_list():
+        """Call insertion_sort on ordered list."""
+        insertion_sort(ordered_list)
+
+    def get_unordered_list():
+        """Call insertion_sort on unordered list."""
+        insertion_sort(unordered_list)
+
+    def get_reversed_list():
+        """Call insertion_sort on reversed list."""
+        insertion_sort(reversed_list)
+
+    print('The Insertion Sort function sorts a list item in comparison to the'
+          ' other items in the list before it.')
+
+    print('Insertion Sort time on an ordered list of 10000: ', timeit.timeit(
+          'get_ordered_list', setup='from __main__ import get_ordered_list'))
+
+    print('Insertion Sort time on an unordered list of 10000: ', timeit.timeit(
+          'get_unordered_list', setup='from __main__ import get_unordered_list'
+          ))
+
+    print('Insertion Sort time on an reversed list of 10000: ', timeit.timeit(
+          'get_reversed_list',
+          setup='from __main__ import get_reversed_list'))
