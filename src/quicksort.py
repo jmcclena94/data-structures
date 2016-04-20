e5e6f36d54fce0add202ee22212cf573efe6ee25
JARED SCARR <JAREDSCARR@GMAIL.COM>
2016-04-20 15:22:04 -0700

    Readme updated and main block added.

--- src/quicksort.py
+++ src/quicksort.py
@@ -28,45 +28,50 @@ def partition(a_list, lo, hi):
     return pivot_point, a_list
 
 
-# if __name__ == '__main__':
-#     """Run in console."""
-#     import timeit
+if __name__ == '__main__':
+    """Run in console."""
+    import timeit
 
-#     ordered_list = []
-#     unordered_list = []
-#     reversed_list = []
+    ordered_list = []
+    unordered_list = []
+    reversed_list = []
 
-#     for item in range(10000):
-#         ordered_list.append(item)
+    for item in range(10000):
+        ordered_list.append(item)
 
-#     for item in range(10000):
-#         unordered_list.append(random.randint(0, 10000))
+    for item in range(10000):
+        unordered_list.append(random.randint(0, 10000))
 
-#     for item in range(10000, 0, -1):
-#         reversed_list.append(item)
+    for item in range(10000, 0, -1):
+        reversed_list.append(item)
 
-#     def get_ordered_list():
-#         """Call merge_sort on ordered list."""
-#         merge_sort(ordered_list)
+    lo = 0
+    ordered_hi = len(ordered_list) - 1
+    unordered_hi = len(unordered_list) - 1
+    reversed_hi = len(reversed_list) - 1
 
-#     def get_unordered_list():
-#         """Call merge_sort on unordered list."""
-#         merge_sort(unordered_list)
+    def get_ordered_list():
+        """Call quick_sort on ordered list."""
+        quick_sort(ordered_list, lo, ordered_hi)
 
-#     def get_reversed_list():
-#         """Call merge_sort on reversed list."""
-#         merge_sort(reversed_list)
+    def get_unordered_list():
+        """Call quick_sort on unordered list."""
+        quick_sort(unordered_list, lo, unordered_hi)
 
-#     print('The Merge Sort function sorts a list item in comparison to the'
-#           ' other items in the list before it.')
+    def get_reversed_list():
+        """Call quick_sort on reversed list."""
+        quick_sort(reversed_list, lo, reversed_hi)
 
-#     print('Merge Sort time on an ordered list of 10000: ', timeit.timeit(
-#           'get_ordered_list', setup='from __main__ import get_ordered_list'))
+    print('The Quicksort function sorts a list by comparing values and '
+          'swapping in place.')
 
-#     print('Merge Sort time on an unordered list of 10000: ', timeit.timeit(
-#           'get_unordered_list', setup='from __main__ import get_unordered_list'
-#           ))
+    print('Quicksort time on an ordered list of 10000: ', timeit.timeit(
+          'get_ordered_list', setup='from __main__ import get_ordered_list'))
 
-#     print('Merge Sort time on an reversed list of 10000: ', timeit.timeit(
-#           'get_reversed_list',
-#           setup='from __main__ import get_reversed_list'))
+    print('Quicksort time on an unordered list of 10000: ', timeit.timeit(
+          'get_unordered_list', setup='from __main__ import get_unordered_list'
+          ))
+
+    print('Quicksort time on an reversed list of 10000: ', timeit.timeit(
+          'get_reversed_list',
+          setup='from __main__ import get_reversed_list'))
