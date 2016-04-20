1c267552f0da9816c60aa20652a6d406b29397af
Joseph McClenahan <jmcclena94@gmail.com>
2016-04-20 14:30:49 -0700

    added name main block

--- src/merge_sort.py
+++ src/merge_sort.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+import random
 
 
 def merge_sort(lst):
@@ -35,3 +36,47 @@ def merge_sort(lst):
                 merge_sublist = merge_sublist + sublist_a[idx_a:]
             flag = False
     return merge_sublist
+
+
+if __name__ == '__main__':
+    """Run in console."""
+    import timeit
+
+    ordered_list = []
+    unordered_list = []
+    reversed_list = []
+
+    for item in range(10000):
+        ordered_list.append(item)
+
+    for item in range(10000):
+        unordered_list.append(random.randint(0, 10000))
+
+    for item in range(10000, 0, -1):
+        reversed_list.append(item)
+
+    def get_ordered_list():
+        """Call merge_sort on ordered list."""
+        merge_sort(ordered_list)
+
+    def get_unordered_list():
+        """Call merge_sort on unordered list."""
+        merge_sort(unordered_list)
+
+    def get_reversed_list():
+        """Call merge_sort on reversed list."""
+        merge_sort(reversed_list)
+
+    print('The Merge Sort function sorts a list item in comparison to the'
+          ' other items in the list before it.')
+
+    print('Merge Sort time on an ordered list of 10000: ', timeit.timeit(
+          'get_ordered_list', setup='from __main__ import get_ordered_list'))
+
+    print('Merge Sort time on an unordered list of 10000: ', timeit.timeit(
+          'get_unordered_list', setup='from __main__ import get_unordered_list'
+          ))
+
+    print('Merge Sort time on an reversed list of 10000: ', timeit.timeit(
+          'get_reversed_list',
+          setup='from __main__ import get_reversed_list'))
